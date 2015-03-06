c02b09b8cd79eeb184f36fe0b0ceada58f27f7e5
vrunoa <vruno.alassia@gmail.com>
2015-03-06 00:38:36 -0300

    add tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,37 @@
-import screenrec from '../lib/';
+import ScreenRec from '../lib/';
 
 describe("Screenrec test", () => {
-   it("Test screenrec", (done) => {
-     throw new Error("not working yet!");
-   });
-});
+  
+  it("Test ScreenRec options are setted", (done) => {
 
+    let app = new ScreenRec({
+      time: 12,
+      output: "output",
+      silent: false,
+      togif: true,
+      wait4key: true
+    });
+
+    let options = app.options;
+    if(options.time != 12) throw new Error("Error setting time param!");
+    if(options.output != "output") throw new Error("Error setting output param!");
+    if(options.silent != false) throw new Error("Error setting silent param!");
+    if(options.togif != true) throw new Error("Error setting togif param!");
+    if(options.wait4key != true) throw new Error("Error setting wait4key param!");
+    done();
+  });
+
+  it("Test ScreenRec stop method", (done) => {
+      
+    let app = new ScreenRec({wait4key:true});
+    app.rec();
+    app.stop();
+    if(app.rec_child.killed != true)
+      throw new Error("Stop method not killing child, run: killall ffmpeg");
+
+    done();
+
+  });
+  
+});
+  
